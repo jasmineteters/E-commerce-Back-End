15b7407b84a6e608d5cd968816127aaf152e0823
Papaya Delight <jasmineteters2020@gmail.com>
2020-12-13 21:49:25 -0700

    added the try catch to the rest of the routes and edited some of the error messages

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,7 @@ router.get("/:id", async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, { include: Product });
     if (!tagData) {
-      res.status(404).json({ message: "No user with this id!" });
+      res.status(404).json({ message: "No tag with this id!" });
       return;
     }
     res.status(200).json(tagData);
@@ -35,8 +35,16 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new tag
-  const newTag = await Tag.create(req.body);
-  res.json(newTag);
+  try {
+    const newTag = await Tag.create(req.body);
+    if (!newTag) {
+      res.status(404).json({ message: "cannot create tag" });
+      return;
+    }
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", async (req, res) => {
